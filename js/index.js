66f38c774af17afc33a33542b8c764d2bf521d56
KamilAminGoIt <158079851+MKamilAmin@users.noreply.github.com>
2024-10-07 20:09:36 +0200

    📦 NEW: Add cart quantity interactivity

--- js/index.js
+++ js/index.js
@@ -73,12 +73,19 @@ document.querySelectorAll(".js-add-to-cart-btn").forEach((btn) => {
             if (productId === item.productId) {
                 matchingItem = item;
             }
-
-            if (matchingItem) {
-                matchingItem.quantity += 1;
-            } else {
-                cart.push({ productId: productId, quantity: 1 });
-            }
         });
+
+        if (matchingItem) {
+            matchingItem.quantity += 1;
+        } else {
+            cart.push({ productId: productId, quantity: 1 });
+        }
+
+        let cartQuantity = 0;
+        cart.forEach((item) => {
+            cartQuantity += item.quantity;
+        });
+
+        document.querySelector(".js-cart-quantity").innerHTML = cartQuantity;
     });
 });
