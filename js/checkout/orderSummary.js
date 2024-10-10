de2eb91d3f18ce4895b0c574d24121d12fa6eee6
KamilAminGoIt <158079851+MKamilAmin@users.noreply.github.com>
2024-10-10 23:34:40 +0200

    📦 NEW: Add automated tests for orderSummary

--- js/checkout/orderSummary.js
+++ js/checkout/orderSummary.js
@@ -23,7 +23,7 @@ export function renderOrderSummary() {
             .format("dddd, MMMM D");
 
         cartSummaryHTML += `   
-    <div class="cart-item-container js-cart-item-container-${
+    <div class="cart-item-container js-cart-item-container js-cart-item-container-${
         matchingProduct.id
     }">
         <div class="delivery-date">
@@ -43,7 +43,9 @@ export function renderOrderSummary() {
                 <div class="product-price">$${formatCurrency(
                     matchingProduct.priceCents
                 )}</div>
-                <div class="product-quantity">
+                <div class="product-quantity js-product-quantity-${
+                    matchingProduct.id
+                }">
                     <span>
                         Quantity:
                         <span class="quantity-label">${cartItem.quantity}</span>
@@ -54,9 +56,9 @@ export function renderOrderSummary() {
                         Update
                     </span>
                     <span
-                        class="delete-quantity-link link-primary js-delete-link" data-product-id="${
+                        class="delete-quantity-link link-primary js-delete-link js-delete-link-${
                             matchingProduct.id
-                        }"
+                        }" data-product-id="${matchingProduct.id}"
                     >
                         Delete
                     </span>
