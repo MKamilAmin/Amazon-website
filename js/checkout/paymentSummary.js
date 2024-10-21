210dec348fb2097677fabd9c82b1df2aca34ab6c
KamilAminGoIt <158079851+MKamilAmin@users.noreply.github.com>
2024-10-21 15:58:57 +0200

    📦 NEW: Create order using backend

--- js/checkout/paymentSummary.js
+++ js/checkout/paymentSummary.js
@@ -2,6 +2,7 @@ import { Cart } from "../../data/cart-oop.js";
 import { getProduct } from "../../data/products.js";
 import { getDeliveryOption } from "../../data/deliveryOptions.js";
 import { formatCurrency } from "../utils/money.js";
+import { addOrder, orders } from "../../data/orders.js";
 
 const cart = new Cart("cart-oop");
 
@@ -59,11 +60,38 @@ export function renderPaymentSummary() {
             )}</div>
         </div>
 
-        <button class="place-order-button button-primary">
+        <button class="place-order-button button-primary js-place-order">
             Place your order
         </button>
     `;
 
     document.querySelector(".js-payment-summary").innerHTML =
         paymentSummaryHTML;
+
+    document
+        .querySelector(".js-place-order")
+        .addEventListener("click", async () => {
+            try {
+                const response = await fetch(
+                    "https://supersimplebackend.dev/orders",
+                    {
+                        method: "POST",
+                        headers: {
+                            "Content-Type": "application/json",
+                        },
+                        body: JSON.stringify({
+                            cart: cart.cartItems,
+                        }),
+                    }
+                );
+
+                const order = await response.json();
+
+                addOrder(order);
+            } catch (error) {
+                console.log("Unexpected error. Try again later.");
+            }
+
+            window.location.href = "orders.html";
+        });
 }
